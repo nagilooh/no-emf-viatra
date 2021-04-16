0b8393fb9594e1fc48135d211cef8497addcf7a1
Ficsor Attila <ficsorattila96@gmail.com>
2021-04-16 16:49:17 +0200

    PQuery builder has rest of the Constraint types

--- extension/org.eclipse.viatra.query.runtime.tabular.generic/src/org/eclipse/viatra/query/runtime/api/generic/BuiltPQuery.java
+++ extension/org.eclipse.viatra.query.runtime.tabular.generic/src/org/eclipse/viatra/query/runtime/api/generic/BuiltPQuery.java
@@ -12,11 +12,18 @@ import java.util.Set;
 import org.eclipse.viatra.query.runtime.matchers.backend.QueryEvaluationHint;
 import org.eclipse.viatra.query.runtime.matchers.psystem.PBody;
 import org.eclipse.viatra.query.runtime.matchers.psystem.PVariable;
+import org.eclipse.viatra.query.runtime.matchers.psystem.basicdeferred.Equality;
 import org.eclipse.viatra.query.runtime.matchers.psystem.basicdeferred.ExportedParameter;
+import org.eclipse.viatra.query.runtime.matchers.psystem.basicdeferred.Inequality;
+import org.eclipse.viatra.query.runtime.matchers.psystem.basicdeferred.NegativePatternCall;
+import org.eclipse.viatra.query.runtime.matchers.psystem.basicenumerables.BinaryReflexiveTransitiveClosure;
+import org.eclipse.viatra.query.runtime.matchers.psystem.basicenumerables.BinaryTransitiveClosure;
+import org.eclipse.viatra.query.runtime.matchers.psystem.basicenumerables.PositivePatternCall;
 import org.eclipse.viatra.query.runtime.matchers.psystem.basicenumerables.TypeConstraint;
 import org.eclipse.viatra.query.runtime.matchers.psystem.queries.BasePQuery;
 import org.eclipse.viatra.query.runtime.matchers.psystem.queries.PParameter;
 import org.eclipse.viatra.query.runtime.matchers.psystem.queries.PParameterDirection;
+import org.eclipse.viatra.query.runtime.matchers.psystem.queries.PQuery;
 import org.eclipse.viatra.query.runtime.matchers.psystem.queries.PVisibility;
 import org.eclipse.viatra.query.runtime.matchers.tuple.Tuples;
 import org.eclipse.viatra.query.runtime.tabular.generic.types.StringExactInstancesKey;
@@ -82,6 +89,65 @@ public class BuiltPQuery extends BasePQuery {
 		bodies.add(body);
 		return this;
 	}
+	
+	// Equality constraint
+	public BuiltPQuery addEquality(String type, String sourceName, String targetName) {
+		PBody body = bodies.peek();
+		PVariable var_source = body.getOrCreateVariableByName(sourceName);
+		PVariable var_target = body.getOrCreateVariableByName(targetName);
+		new Equality(body, var_source, var_target);
+		return this;
+	}
+	
+	// Inequality constraint
+	public BuiltPQuery addInequality(String type, String sourceName, String targetName) {
+		PBody body = bodies.peek();
+		PVariable var_source = body.getOrCreateVariableByName(sourceName);
+		PVariable var_target = body.getOrCreateVariableByName(targetName);
+		new Inequality(body, var_source, var_target);
+		return this;
+	}
+	
+	// Positive pattern call
+	public BuiltPQuery addPatternCall(PQuery query, String... names) {
+		PBody body = bodies.peek();
+		PVariable[] vars = new PVariable[names.length];
+		for (int i = 0; i < names.length; i++) {
+			vars[i] = body.getOrCreateVariableByName(names[i]);
+		}
+		new PositivePatternCall(body, Tuples.flatTupleOf(vars), query);
+		return this;
+	}
+	
+	// Negative pattern call
+	public BuiltPQuery addNegativePatternCall(PQuery query, String... names) {
+		PBody body = bodies.peek();
+		PVariable[] vars = new PVariable[names.length];
+		for (int i = 0; i < names.length; i++) {
+			vars[i] = body.getOrCreateVariableByName(names[i]);
+		}
+		new NegativePatternCall(body, Tuples.flatTupleOf(vars), query);
+		return this;
+	}
+	
+	// Binary transitive closure pattern call
+	public BuiltPQuery addBinaryTransitiveClosure(PQuery query, String sourceName, String targetName) {
+		PBody body = bodies.peek();
+		PVariable var_source = body.getOrCreateVariableByName(sourceName);
+		PVariable var_target = body.getOrCreateVariableByName(targetName);
+		new BinaryTransitiveClosure(body, Tuples.flatTupleOf(var_source, var_target), query);
+		return this;
+	}
+	
+	// Binary reflexive transitive closure pattern call
+	public BuiltPQuery addBinaryReflexiveTransitiveClosure(PQuery query, String sourceName, String targetName) {
+		PBody body = bodies.peek();
+		PVariable var_source = body.getOrCreateVariableByName(sourceName);
+		PVariable var_target = body.getOrCreateVariableByName(targetName);
+		new BinaryReflexiveTransitiveClosure(body, Tuples.flatTupleOf(var_source, var_target), query, new StringExactInstancesKey(query.getParameters().get(0).getTypeName()));
+		return this;
+	}
+	
 
 	@Override
 	public Set<PBody> doGetContainedBodies() {
